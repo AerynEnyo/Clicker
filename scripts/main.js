674af9f1118eed4a950d68cf5b0248484caa3fa3
AerynEnyo <142051937+AerynEnyo@users.noreply.github.com>
2024-06-25 05:17:51 -0230

    v0.0.1.1
    Added save/load functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,8 @@
 // Variables
-let multi = 1;
-let money = 0;
+let v = {
+    multi: 1,
+    money: 0,
+}
 
 let add1cost = 10;
 let add25cost = 20;
@@ -8,36 +10,44 @@ let add25cost = 20;
 var timer = setInterval(moneypersec, 1000);
 var timer = setInterval(UIUpdate, 67);
 
+function save(){
+    localStorage.setItem("variables", JSON.stringify(v))
+}
+
+function load(){
+    v = JSON.parse(localStorage.getItem("variables"))
+}
+
+function moneypersec(){
+    v.money += 1 * v.multi;
+}
+
 function add1multi(){   
-    if (money >= add1cost) {
-        money -= add1cost
-        multi++;
+    if (v.money >= add1cost) {
+        v.money -= add1cost
+        v.multi++;
     }
 }
 
 function add25multi(){   
-    if (money >= add25cost) {
-        money -= add25cost
-        multi += 2.5;
+    if (v.money >= add25cost) {
+        v.money -= add25cost
+        v.multi += 2.5;
     }
 }
 
-function moneypersec(){
-    money += 1 * multi;
-}
-
 function UIUpdate(){
-    document.getElementById("clickcount").innerHTML = 'Multiplier: ' + multi;
-    document.getElementById("money").innerHTML = 'Money: ' + money;
+    document.getElementById("clickcount").innerHTML = 'Multiplier: ' + v.multi;
+    document.getElementById("money").innerHTML = 'Money: ' + v.money;
 
-    if (money >= add1cost) {
+    if (v.money >= add1cost) {
         document.getElementById("add1").toggleAttribute("disabled", false);
     }
     else {
         document.getElementById("add1").toggleAttribute("disabled", true);
     }
 
-    if (money >= add25cost) {
+    if (v.money >= add25cost) {
         document.getElementById("add25").toggleAttribute("disabled", false);
     }
     else {
